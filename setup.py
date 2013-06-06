a844c85efcc87d255d1191a627de8f735268c3f6
eofs <tomi@madlab.fi>
2013-06-06 16:53:10 +0300

    use find_package() from setuptools instead of custom one

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 import os
 import re
 
-from setuptools import setup
-
+from setuptools import setup, find_packages
 
 name = 'aws'
 package = 'aws'
@@ -18,14 +17,6 @@ def get_version(package):
     init_py = open(os.path.join(package, '__init__.py')).read()
     return re.search("^__version__ = ['\"]([^'\"]+)['\"]", init_py, re.MULTILINE).group(1)
 
-def get_packages(package):
-    """
-    Return root package and all sub-packages.
-    """
-    return [dirpath
-            for dirpath, dirnames, filenames in os.walk(package)
-            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
-
 setup(name=name,
       version=get_version(package),
       description=description,
@@ -33,7 +24,7 @@ setup(name=name,
       author=author,
       author_email=author_email,
       license='BSD',
-      packages=get_packages(package),
+      packages=find_packages(),
       zip_safe=False,
       requires=['boto', 'fabric'],
       entry_points={
