2916ee1946f2a69ad7f4fdeb658a27a8befb38d0
eofs <tomi@madlab.fi>
2013-06-07 10:37:50 +0300

    New git-style command line syntax

--- aws/main.py
+++ aws/main.py
@@ -4,7 +4,8 @@ import argparse
 import getpass
 
 from fabric import api as fab
-from fabric.main import parse_arguments
+from fabric import state as fab_state
+from fabric.main import find_fabfile, load_fabfile, parse_arguments
 
 from aws import __version__
 from libaws.conf import settings
@@ -22,6 +23,7 @@ def list_regions(service):
             'endpoint': region.endpoint,
         }
 
+
 def ec2_fab(service, args):
     """
     Run Fabric commands against EC2 instances
@@ -34,45 +36,50 @@ def ec2_fab(service, args):
     fab.env.user = settings.get('SSH', 'USER', getpass.getuser())
     fab.env.parallel = True
 
-    def import_file(file):
-        return __import__(file)
+    fabfile = find_fabfile(args.file)
+    if not fabfile:
+        print 'Couldn\'t find any fabfiles!'
+        return
 
-    module_name = args.fab[0]
-    module = import_file(module_name)
+    fab.env.real_fabile = fabfile
+    docstring, callables, default = load_fabfile(fabfile)
+    fab_state.commands.update(callables)
 
-    commands_to_run = parse_arguments(args.fab[1:])
+    commands_to_run = parse_arguments(args.methods)
     for name, args, kwargs, arg_hosts, arg_roles, arg_exclude_hosts in commands_to_run:
-        method = getattr(module, name, None)
-        if method is not None:
-            fab.execute(method,
-                        hosts=arg_hosts,
-                        roles=arg_roles,
-                        exclude_hosts=arg_exclude_hosts,
-                        *args, **kwargs
-            )
+        fab.execute(name,
+                    hosts=arg_hosts,
+                    roles=arg_roles,
+                    exclude_hosts=arg_exclude_hosts,
+                    *args, **kwargs
+        )
 
-def ec2_service_handler(parser, args):
+
+def ec2_list_handler(parser, args):
     service = EC2Service(settings)
-    if args.list:
-        list = service.list(elb=args.elb)
-        for x in list:
-            for i in x.instances:
+    if 'regions' == args.type:
+        list_regions(service)
+    else:
+        reservations = service.list(elb=args.elb)
+        for r in reservations:
+            for i in r.instances:
                 state = ('       ' if i.state == 'stopped' else i.state)
                 print '[%s] [%s] [%s] %s:\t%s' % (state, i.instance_type, i.id, i.key_name, i.dns_name)
-    elif args.regions:
-        list_regions(service)
-    elif args.fab:
-        if len(args.fab) < 2:
-            parser.error('Not enough parameters provided for --fab.')
-        ec2_fab(service, args)
 
-def elb_service_handler(parser, args):
+
+def ec2_fab_handler(parser, args):
+    service = EC2Service(settings)
+    ec2_fab(service, args)
+
+
+def elb_list_handler(parser, args):
     service = ELBService(settings)
-    if args.list:
-        for x in service.list():
-            print '%s: %s' % (x.name, x.dns_name)
-    elif args.regions:
+    if 'regions' == args.type:
         list_regions(service)
+    else:
+        for b in service.list():
+            print '%s: %s' % (b.name, b.dns_name)
+
 
 def main():
     """
@@ -83,18 +90,42 @@ def main():
                                 version=__version__)
     subparsers = p.add_subparsers(help='Select Amazon AWS service to use')
 
-    ec2_service = subparsers.add_parser('ec2', help='Amazon EC2')
-    ec2_service.add_argument('--list', '-l', action='store_true', help='List instances')
-    ec2_service.add_argument('--regions', '-r', action='store_true', help='List regions')
-    ec2_service.add_argument('--elb', '-e', help='Filter instances inside this ELB instance')
-    ec2_service.add_argument('--fab', '-f', nargs='+', metavar=('fabfile method', ':arg1,arg2=val2,host=foo,hosts=\'h1;h2\','),
-                             help='Run Fabric tasks in EC2 instances. You can pass multiple arguments for your fabfile.')
-    ec2_service.set_defaults(func=ec2_service_handler)
+    # Elastic Cloud Computing
+    ec2_service = subparsers.add_parser('ec2', help='Amazon Elastic Compute Cloud')
+    ec2_subparsers = ec2_service.add_subparsers(help='Perform action')
 
-    elb_service = subparsers.add_parser('elb', help='Amazon Elastic Load Balancer')
-    elb_service.add_argument('--list', '-l', action='store_true', help='List load balancers')
-    elb_service.add_argument('--regions', '-r', action='store_true', help='List regions')
-    elb_service.set_defaults(func=elb_service_handler)
+    ec2_service_list = ec2_subparsers.add_parser('list', help='List items')
+    ec2_service_list.add_argument('--elb', '-e', help='Filter instances inside this ELB instance')
+    ec2_service_list.add_argument('--type', default='instances', choices=['instances', 'regions'],
+                                  help='List items of this type')
+    ec2_service_list.set_defaults(func=ec2_list_handler)
+
+    ec2_service_fab = ec2_subparsers.add_parser('fab', help='Run Fabric commands')
+    ec2_service_fab.add_argument('--elb', '-e', help='Run against EC2 instances for this ELB')
+    ec2_service_fab.add_argument('--file', '-f', nargs='+', help='Define fabfile to use')
+    ec2_service_fab.add_argument('methods',
+                                 metavar='method:arg1,arg2=val2,host=foo,hosts=\'h1;h2\',',
+                                 nargs='+',
+                                 help='Run Fabric tasks in EC2 instances. You can pass multiple arguments for your fabfile.')
+    ec2_service_fab.set_defaults(func=ec2_fab_handler)
+
+    # ec2_service_start = ec2_subparsers.add_parser('start', help='Start instance')
+    # ec2_service_stop = ec2_subparsers.add_parser('stop', help='Stop instance')
+    # ec2_service_terminate = ec2_subparsers.add_parser('terminate', help='Terminate instance')
+
+    # Elastic Load Balancing
+    elb_service = subparsers.add_parser('elb', help='Amazon Elastic Load Balancing')
+    elb_subparsers = elb_service.add_subparsers(help='Perform action')
+
+    elb_service_list = elb_subparsers.add_parser('list', help='List items')
+    elb_service_list.add_argument('--type', default='balancers', choices=['balancers', 'regions'],
+                                  help='List items of this type')
+    elb_service_list.set_defaults(func=elb_list_handler)
+
+    # elb_service_create = elb_subparsers.add_parser('create', help='Create new Load Balancer')
+    # elb_service_delete = elb_subparsers.add_parser('delete', help='Delete Load Balancer')
+    # elb_service_register = elb_subparsers.add_parser('register', help='Register EC2 instance')
+    # elb_service_zone = elb_subparsers.add_parser('zone', help='Enable or disable region')
 
     arguments = p.parse_args()
     arguments.func(p, arguments)
